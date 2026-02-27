4c3f012ac6e4ddce566424e969332654e0684315
Noah MacDonald <nmacdonald2113@gmail.com>
2026-02-27 15:34:15 -0500

    export and copy to clipboard functioning. import not currently functioning

--- src/app/components/file-panel/file-panel.component.ts
+++ src/app/components/file-panel/file-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, inject } from '@angular/core';
+import { EditorStateService } from '../../services/editor-state.service';
 
 @Component({
   selector: 'app-file-panel',
@@ -8,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrl: './file-panel.component.css'
 })
 export class FilePanelComponent {
+  editorStateService = inject(EditorStateService);
 
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+
+  importFile() {
+    this.fileInput.nativeElement.click();
+  }
+
+  handleFile(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if(!file) return;
+
+    const reader = new FileReader();
+    reader.onload=()=>{
+      const text = reader.result as string;
+      this.editorStateService.setContent(text);
+    };
+    reader.readAsText(file);
+  }
+
+  exportFile() {
+    const content = this.editorStateService.getContent();
+    const blob = new Blob([content], {type: 'text/markdown'});
+    const url= URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'README.md';
+    a.click();
+
+    URL.revokeObjectURL(url);
+  }
+
+  async copyToClipboard() {
+    const content = this.editorStateService.getContent();
+    await navigator.clipboard.writeText(content);
+  } 
 }
