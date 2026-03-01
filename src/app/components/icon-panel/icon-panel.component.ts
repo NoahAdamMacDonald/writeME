a4446b182b12acb98a718a8ea81566853e8e9c69
Noah MacDonald <nmacdonald2113@gmail.com>
2026-02-28 20:05:48 -0500

    emoji panel added

--- src/app/components/icon-panel/icon-panel.component.ts
+++ src/app/components/icon-panel/icon-panel.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import emojiData from '../../../assets/emoji.json';
 
 @Component({
   selector: 'app-icon-panel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './icon-panel.component.html',
   styleUrl: './icon-panel.component.css'
 })
-export class IconPanelComponent {
+export class IconPanelComponent implements OnInit {
+  categories: any[] = [];
 
+  copy(text: string) {
+    navigator.clipboard.writeText(text);
+  }
+
+  ngOnInit() {
+    this.categories = (emojiData as any).categories;
+  }
 }
