6be7df0e40b03edc686cd5610477a0b0ef6a33d2
Noah MacDonald <nmacdonald2113@gmail.com>
2026-03-06 16:07:57 -0500

    added dark mode icon. need to add styling changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  isDarkMode = true;
 
+  ngOnInit(): void {
+    const saved = localStorage.getItem('theme');
+
+    if (saved === 'dark') {
+      this.isDarkMode = true;
+    } else if (saved === 'light') {
+      this.isDarkMode = false;
+    } else {
+      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      this.isDarkMode = mediaQuery.matches;
+    }
+
+    this.applyTheme();
+  }
+
+  applyTheme() {
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+    this.applyTheme();
+  }
 }
