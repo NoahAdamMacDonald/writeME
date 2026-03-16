79a18eac263fd6b2abab1cc2b810bc0034accff0
Noah MacDonald <nmacdonald2113@gmail.com>
2026-03-16 15:52:53 -0400

    service added

--- src/app/services/ocr.service.ts
+++ src/app/services/ocr.service.ts
@@ -7,7 +7,7 @@ import { createWorker } from 'tesseract.js';
 export class OcrService {
   async extractText(file: File): Promise<string> {
     const worker = await createWorker('eng');
-    
+
     const { data } = await worker.recognize(file);
     await worker.terminate();
 
