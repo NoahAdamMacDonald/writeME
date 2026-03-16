469ad81317433e18bf9a93872a26b6e4fc841b32
Noah MacDonald <nmacdonald2113@gmail.com>
2026-03-16 15:52:30 -0400

    service to convert an image to text

--- src/app/services/ocr.service.ts
+++ src/app/services/ocr.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OcrService {
+  async extractText(file: File): Promise<string> {
+    const worker = await createWorker('eng');
+    
+    const { data } = await worker.recognize(file);
+    await worker.terminate();
+
+    return data.text;
+  }
 
   constructor() { }
 }
