6181d03d1955af58e1b50bfeab41c186f6258503
Noah MacDonald <nmacdonald2113@gmail.com>
2026-03-16 16:29:10 -0400

    key added, public free no need to hide. will swap out for proper handling if switching to paid

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  groqKey: 'gsk_your_dev_key',
+  groqKey: 'gsk_mH0RSm359rclm95QSpxAWGdyb3-FY98xMyYd0fjhqxNWhodTnoDJn',
   groqModel: 'llama-3.1-8b-instant',
   useProxy: false,
 };
